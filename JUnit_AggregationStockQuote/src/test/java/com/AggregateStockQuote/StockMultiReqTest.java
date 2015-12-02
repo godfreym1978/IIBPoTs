0cebe6738475d93ab3fee8a1acc9c0dc07bb5bef
Godfrey Menezes <godfreym@gmail.com>
2015-12-02 10:05:04 -0500

    First commit.

---
+++ JUnit_AggregationStockQuote/src/test/java/com/AggregateStockQuote/StockMultiReqTest.java
@@ -0,0 +1,46 @@
+package com.AggregateStockQuote;
+
+import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.custommonkey.xmlunit.*;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.testing.mq.Util;
+
+public class StockMultiReqTest extends XMLTestCase {
+
+	@Test
+	public void test() throws IOException, InterruptedException, SAXException {
+		// fail("Not yet implemented");
+		String inputFile = System.getProperty("user.dir")
+				+ "/InputFile/StockQuoteReqMulti.xml";
+		String outputFile = System.getProperty("user.dir")
+				+ "/OutputFile/StockQuoteRepMulti.xml";
+		String controlFile = System.getProperty("user.dir")
+				+ "/ExpectedFile/StockQuoteRepMulti.xml";
+
+		Util newUtil = new Util();
+		newUtil.writeMessageToQueue("WQM.INT", "AGG.REQ", inputFile);
+		TimeUnit.SECONDS.sleep(7);
+		newUtil.readMessage("WQM.INT", "AGG.REP", outputFile);
+
+		String controlXML = newUtil.readFile(controlFile);
+		String testXML = newUtil.readFile(outputFile);
+
+		DifferenceListener myDifferenceListener = new IgnoreTextAndAttributeValuesDifferenceListener();
+		Diff myDiff = new Diff(controlXML, testXML);
+		myDiff.overrideDifferenceListener(myDifferenceListener);
+		assertTrue("test XML matches control skeleton XML " + myDiff,
+				myDiff.similar());
+
+		// assertTrue("Comparing test xml to control xml", controlXML, testXML);
+
+	}
+}
---
+++ JUnit_AggregationStockQuote/src/test/java/com/AggregateStockQuote/StockSingleReqTest.java
@@ -0,0 +1,40 @@
+package com.AggregateStockQuote;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.custommonkey.xmlunit.*;
+
+import org.junit.Test;
+import org.xml.sax.SAXException;
+
+import com.testing.mq.Util;
+
+public class StockSingleReqTest extends XMLTestCase {
+
+	@Test
+	public void test() throws IOException, InterruptedException, SAXException {
+		// fail("Not yet implemented");
+		String inputFile = System.getProperty("user.dir")
+				+ "/InputFile/StockQuoteReqSingle.xml";
+		String outputFile = System.getProperty("user.dir")
+				+ "/OutputFile/StockQuoteRepSingle.xml";
+		String controlFile = System.getProperty("user.dir")
+				+ "/ExpectedFile/StockQuoteRepSingle.xml";
+
+		Util newUtil = new Util();
+		newUtil.writeMessageToQueue("WQM.INT", "AGG.REQ", inputFile);
+		TimeUnit.SECONDS.sleep(5);
+		newUtil.readMessage("WQM.INT", "AGG.REP", outputFile);
+
+		String controlXML = newUtil.readFile(controlFile);
+		String testXML = newUtil.readFile(outputFile);
+
+		DifferenceListener myDifferenceListener = new IgnoreTextAndAttributeValuesDifferenceListener();
+		Diff myDiff = new Diff(controlXML, testXML);
+		myDiff.overrideDifferenceListener(myDifferenceListener);
+		assertTrue("test XML matches control skeleton XML " + myDiff,
+				myDiff.similar());
+		// assertTrue("Comparing test xml to control xml", controlXML, testXML);
+
+	}
+}
